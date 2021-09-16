71e5b6b4e1abb90fc6035200ec766275d6b3d8ed
driedel152 <45524258+driedel152@users.noreply.github.com>
2021-09-15 21:31:10 -0500

    We got meshes
    and some cool customizations

---
+++ Assets/_Scripts/TerrainGeneration/MeshGenerator.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MeshGenerator
+{
+
+    public static MeshData GenerateTerrainMesh(float[,] noiseMap)
+	{
+        float scale = 10;
+        int length = noiseMap.GetLength(0);
+        float topLeftX = (length - 1) / -2f;
+        float topLeftZ = (length - 1) / 2f;
+        int area = length * length; // assuming it's a square
+        int maxMeshTriangles = area * 2;
+        int maxMeshVertices = maxMeshTriangles * 3;
+        MeshData meshData = new MeshData(maxMeshVertices, maxMeshTriangles);
+
+		int vertexIndex = 0;
+
+        for (int x = 0; x < length; x++)
+        {
+            for (int y = 0; y < length; y++)
+            {
+                Vector3 vertex = new Vector3(topLeftX + x, noiseMap[x, y] * scale, topLeftZ - y);
+                meshData.AddVertex(vertex, new Vector2(x/(float)length, y/(float)length), vertexIndex);
+
+                if(x < length - 1 && y < length - 1)
+                {
+                    meshData.AddTriangle(vertexIndex + length, vertexIndex + length + 1, vertexIndex);
+                    meshData.AddTriangle(vertexIndex + 1, vertexIndex, vertexIndex + length + 1);
+                }
+
+                vertexIndex++;
+            }
+        }
+
+		return meshData;
+
+	}
+}
+
+public class MeshData
+{
+	Vector3[] vertices;
+	int[] triangles;
+	Vector2[] uvs;
+
+	int triangleIndex;
+
+	public MeshData(int maxNumVertices, int maxNumTriangles)
+	{
+		vertices = new Vector3[maxNumVertices];
+		uvs = new Vector2[maxNumVertices];
+		triangles = new int[maxNumTriangles * 3];
+	}
+
+	public void AddVertex(Vector3 vertexPosition, Vector2 uv, int vertexIndex)
+	{
+		vertices[vertexIndex] = vertexPosition;
+		uvs[vertexIndex] = uv;
+	}
+
+	public void AddTriangle(int a, int b, int c)
+	{
+		triangles[triangleIndex] = a;
+		triangles[triangleIndex + 1] = b;
+		triangles[triangleIndex + 2] = c;
+		triangleIndex += 3;
+	}
+
+	public Mesh CreateMesh()
+	{
+		Mesh mesh = new Mesh();
+		//mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.uv = uvs;
+
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+
+		return mesh;
+	}
+
+}
--- Assets/_Scripts/TerrainGeneration/TerrainGenerator.cs
+++ Assets/_Scripts/TerrainGeneration/TerrainGenerator.cs
@@ -4,22 +4,68 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    [Range(2, 16)]
+    [SerializeField]
+    int size;
     [SerializeField]
     NoiseSettings noiseSettings;
 
+    [SerializeField] float scaleHeight;
+    [SerializeField] bool applyCornerFallOff;
+    [SerializeField] float cornerFallOffPower;
+    [SerializeField] bool applyTerraces;
+    [SerializeField] int terraceFrequency;
+
     public bool autoUpdate;
 
-    [SerializeField]
-    Material mapMaterial;
-
-    float[,] noise;
+    [SerializeField] Material material;
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] MeshRenderer meshRender;
 
     public void GenerateMap()
     {
-        noise = Noise.GenerateNoiseMap(10, noiseSettings, Vector2.zero);
+        float[,] noise = Noise.GenerateNoiseMap(size, noiseSettings, Vector2.zero);
+        AddEffectsToNoise(noise);
+
         Texture texture = TextureGenerator.TextureFromNoiseMap(noise);
+        material.mainTexture = texture;
 
-        mapMaterial.mainTexture = texture;
+        meshFilter.sharedMesh = MeshGenerator.GenerateTerrainMesh(noise).CreateMesh();
+        meshRender.sharedMaterial.mainTexture = texture;
+    }
 
+    public void AddEffectsToNoise(float[,] noise)
+    {
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                float effect = 1;
+
+                // island fall off
+                float distFromCenterX = Mathf.Abs(x - size / 2);
+                float distFromCenterY = Mathf.Abs(y - size / 2);
+                float distFromCenter = Mathf.Sqrt(distFromCenterX * distFromCenterX + distFromCenterY * distFromCenterY);
+                effect *= Mathf.InverseLerp(size / 2, 0, distFromCenter);
+
+                // corner fall off
+                if (applyCornerFallOff)
+                {
+                    effect *= Mathf.Pow(Mathf.InverseLerp(size, 0, x), cornerFallOffPower);
+                    effect *= Mathf.Pow(Mathf.InverseLerp(0, size, y), cornerFallOffPower);
+                }
+
+                // scale height
+                effect *= Mathf.Pow(scaleHeight, applyCornerFallOff ? cornerFallOffPower : 1);
+
+                // terraces
+                if (applyTerraces)
+                {
+                    effect = Mathf.Floor(effect * terraceFrequency) / terraceFrequency;
+                }
+
+                noise[x, y] *= effect;
+            }
+        }
     }
 }
