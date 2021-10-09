3d897ff2702c9df9b0d226c0c3fb42deecac6293
driedel152 <45524258+driedel152@users.noreply.github.com>
2021-10-08 20:40:27 -0500

    Added basic UI to select and deploy units

--- Assets/_Scripts/Controllers/UIController.cs
+++ Assets/_Scripts/Controllers/UIController.cs
@@ -2,13 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField] Canvas canvas;
+    [SerializeField] List<string> unitOptions; // TODO: determine type
+
+    List<Image> unitImages;
+
     PlayerController playerController;
+
+    string SelectedUnitType
+    {
+        get => unitOptions[_selectedUnitIndex];
+    }
+
+    int SelectedUnitIndex
+    {
+        get => _selectedUnitIndex;
+        set
+        {
+            _selectedUnitIndex = value;
+            if (_selectedUnitIndex >= unitOptions.Count)
+            {
+                _selectedUnitIndex = 0;
+            }
+            if (_selectedUnitIndex < 0)
+            {
+                _selectedUnitIndex = unitOptions.Count - 1;
+            }
+
+            foreach(Image image in unitImages)
+            {
+                image.color = Color.white;
+            }
+
+            unitImages[_selectedUnitIndex].color = Color.yellow;
+        }
+    }
+
+    int _selectedUnitIndex;
+
     private void Start()
     {
+        SetUpUnitOptionImages();
+
         playerController = GetComponent<PlayerController>();
+        SelectedUnitIndex = 0;
+    }
+
+    void SetUpUnitOptionImages()
+    {
+        unitImages = new List<Image>();
+        int imageSize = 100;
+        float offset = -unitOptions.Count * imageSize / 2f;
+        for(int i=0; i<unitOptions.Count; i++)
+        {
+            Image image = new GameObject().AddComponent<Image>(); // TODO: Add real images
+            image.transform.SetParent(canvas.transform, false);
+            image.transform.localPosition = new Vector3(offset + imageSize * i, 150, 0);
+            image.rectTransform.sizeDelta = new Vector2(imageSize, imageSize);
+            image.rectTransform.pivot = Vector2.zero;
+            image.name = "Unit Selection";
+
+            unitImages.Add(image);
+        }
+    }
+
+    public void OnDeployUnit(InputAction.CallbackContext context)
+    {
+        if (Game.instance.IsPlayerRegistered(playerController))
+        {
+            if (context.performed)
+            {
+                Debug.Log("Deploying: " + SelectedUnitType);
+            }
+        }
+    }
+
+    public void OnUnitSelectChange(InputAction.CallbackContext context)
+    {
+        if (Game.instance.IsPlayerRegistered(playerController))
+        {
+            if (context.performed)
+            {
+                SelectedUnitIndex += (int)context.ReadValue<float>();
+            }
+        }
     }
 
     public void OnUnpause(InputAction.CallbackContext context)
