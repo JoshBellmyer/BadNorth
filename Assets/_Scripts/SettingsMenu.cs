7cf38f1157e1e30ce45016c8bb9b28601bbae057
driedel152 <45524258+driedel152@users.noreply.github.com>
2021-10-16 16:15:53 -0500

    Added better controller support for settings menu

--- Assets/_Scripts/SettingsMenu.cs
+++ Assets/_Scripts/SettingsMenu.cs
@@ -7,6 +7,10 @@ public class SettingsMenu : PlayerMenu
     [SerializeField] Text zoomSensitivity;
     [SerializeField] Text rotateSensitivity;
 
+    const float CURSOR_SENSITIVITY_STEP = 0.1f;
+    const float ZOOM_SENSITIVITY_STEP = 0.01f;
+    const float ROTATE_SENSITIVITY_STEP = 5f;
+
     protected new void Start()
     {
         base.Start();
@@ -31,5 +35,36 @@ public class SettingsMenu : PlayerMenu
     public void Back()
     {
         playerUIManager.SwitchMenu(typeof(PauseMenu));
+        LoadSettingsValues(); // revert changes if neccessary
+    }
+
+    public void IncreaseCursorSensitivity()
+    {
+        cursorSensitivity.text = float.Parse(cursorSensitivity.text) + CURSOR_SENSITIVITY_STEP + "";
+    }
+
+    public void DecreaseCursorSensitivity()
+    {
+        cursorSensitivity.text = float.Parse(cursorSensitivity.text) - CURSOR_SENSITIVITY_STEP + "";
+    }
+
+    public void IncreaseZoomSensitivity()
+    {
+        zoomSensitivity.text = float.Parse(zoomSensitivity.text) + ZOOM_SENSITIVITY_STEP + "";
+    }
+
+    public void DecreaseZoomSensitivity()
+    {
+        zoomSensitivity.text = float.Parse(zoomSensitivity.text) - ZOOM_SENSITIVITY_STEP + "";
+    }
+
+    public void IncreaseRotateSensitivity()
+    {
+        rotateSensitivity.text = float.Parse(rotateSensitivity.text) + ROTATE_SENSITIVITY_STEP + "";
+    }
+
+    public void DecreaseRotateSensitivity()
+    {
+        rotateSensitivity.text = float.Parse(rotateSensitivity.text) - ROTATE_SENSITIVITY_STEP + "";
     }
 }
