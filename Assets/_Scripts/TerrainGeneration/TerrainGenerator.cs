d1c36e3b451392112df010e1d4f8c11fd15c19fd
driedel152 <45524258+driedel152@users.noreply.github.com>
2022-04-30 01:04:40 -0500

    Revert "Fixed race condition"
    This reverts commit c75375c27c2a822eb9ba837a84ec10d5b210915a.

--- Assets/_Scripts/TerrainGeneration/TerrainGenerator.cs
+++ Assets/_Scripts/TerrainGeneration/TerrainGenerator.cs
@@ -27,6 +27,7 @@ public class TerrainGenerator : NetworkBehaviour, ISavable
 
     public void SetUpMap()
     {
+        random = new System.Random(seed);
         LoadTileSet();
 
         if (Game.isHost && Game.online)
@@ -36,7 +37,6 @@ public class TerrainGenerator : NetworkBehaviour, ISavable
         }
         else if (!Game.online || !Application.isPlaying)
         {
-            random = new System.Random(seed);
             seed = randomizeSeed ? Random.Range(int.MinValue, int.MaxValue) : seed;
             StartCoroutine(GenerateMap(seed, tileSetName));
         }
@@ -61,7 +61,6 @@ public class TerrainGenerator : NetworkBehaviour, ISavable
     [ClientRpc]
     private void SendGenerationInfoClientRpc(int seed, string tileSetName)
     {
-        random = new System.Random(seed);
         StartCoroutine(GenerateMap(seed, tileSetName));
     }
 
